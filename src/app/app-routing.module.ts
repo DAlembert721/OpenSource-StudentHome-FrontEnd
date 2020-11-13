8ee768917e095ed284520d0f5445105b34851570
Dalembert Monzon <DDMU@users.noreply.github.com>
2020-11-13 00:11:13 -0500

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,14 @@ import {PropertiesComponent} from './pages/properties/properties.component';
 import {AddPropertyComponent} from './pages/add-property/add-property.component';
 import {PropertyDetailsComponent} from './pages/property-details/property-details.component';
 import {AppComponent} from './app.component';
- import {StudentProfileComponent} from './pages/student-profile/student-profile.component';
+import {StudentProfileComponent} from './pages/student-profile/student-profile.component';
 import {RequestListComponent} from './pages/request-list/request-list.component';
 import {ContractListComponent} from './pages/contract-list/contract-list.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full'},
-  { path: '/home', component: AppComponent},
-  { path: 'landlords', redirectTo: '/users/61/landlords/101', pathMatch: 'full'},
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: AppComponent},
+  { path: 'landlords', redirectTo: 'users/61/landlords/101', pathMatch: 'full'},
   { path: 'landlords/:id', component: LandlordProfileComponent },
   { path: 'users/:userId/landlords/:landlordId', component: LandlordProfileComponent},
   { path: 'landlords/:landlordId/properties', component: PropertiesComponent},
